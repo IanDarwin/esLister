12657b87f01666496561e877093fc6b6ab2b239f
Ian Darwin <ian@darwinsys.com>
2023-03-15 11:49:56 -0400

    +toMap()

--- lib/model/Item.dart
+++ lib/model/Item.dart
@@ -10,4 +10,14 @@ class Item {
   double? value;
 
   Item(this.name, {images, description, location, value});
+
+  Map<String,dynamic> toMap() {
+    return {
+      "name": name,
+      "images": images,
+      "description": description,
+      "location": location,
+      "value": value,
+    };
+  }
 }
